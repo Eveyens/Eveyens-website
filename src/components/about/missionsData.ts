c7e45fcf6e86c6b9948d69a5aefb3c0006ac3c0e
Eveyens <pironalexandre45@gmail.com>
2025-03-16 23:10:46 +0100

    Create missionsData.ts

---
+++ src/components/about/missionsData.ts
@@ -0,0 +1,40 @@
+const missions = [
+  {
+    client: "EDF Hydro CODIR GEH",
+    year: "2024",
+    description: "Accompagnement du CODIR sur ses modes de fonctionnement",
+    link: "https://bit.ly/3vlR4hc"
+  },
+  {
+    client: "Ville de Bezons",
+    year: "2024",
+    description: "Accompagnement pour le développement des compétences managériales des cadres de la ville",
+    link: "https://bit.ly/3K79wxS"
+  },
+  {
+    client: "MIEPEEC Maroc",
+    year: "2023",
+    description: "Programme « Direction Femmes » pour le développement du leadership de 100 femmes cadres",
+    link: "https://bit.ly/3HeSXj0"
+  },
+  {
+    client: "EDF Hydro Business Unite",
+    year: "2023",
+    description: "Conception et animation de séminaires pour ED, CODIR et CODIR Élargi sur la vision 2030",
+    link: "https://bit.ly/46UIgfo"
+  },
+  {
+    client: "Chicanrie",
+    year: "2023",
+    description: "Ateliers découvertes pour appréhender les conflits",
+    link: "https://bit.ly/49cRncy"
+  },
+  {
+    client: "EDF Hydro",
+    year: "2022",
+    description: "Cursus de professionnalisation des Encadrants",
+    link: "http://bit.ly/40mbgKm"
+  }
+];
+
+export default missions;
