4879cd86206ff40472fc71f468c8ab5a9731c0d0
Eveyens <pironalexandre45@gmail.com>
2025-04-01 21:10:23 +0200

    Maman rale

--- src/components/about/missionsData.ts
+++ src/components/about/missionsData.ts
@@ -1,7 +1,7 @@
 
 const missions = [
   { 
-    client: "EDF Hydro Rencontres GEH SMB",
+    client: "EDF Hydro Rencontres SMB",
     year: "2025",
     description: "Co-construire des solutions concrètes grâce au dialogue et à l’engagement.",
     link: "https://www.linkedin.com/posts/elena-chilova-piron-pcc_engagement-collaboration-leadership-activity-7304876779402354691-i8-L?utm_source=share&utm_medium=member_desktop&rcm=ACoAAFKCPEABbi4cViMyEgIxB0sl8YLissAc1KY"
